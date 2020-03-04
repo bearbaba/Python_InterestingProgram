54d2353927d351ee47b7ac6d4c246e601d82d367
bearbaba <2200522850@qq.com>
2020-03-04 23:50:45 +0800

    增加了readme中的解释内容

--- Python_plane/plane.py
+++ Python_plane/plane.py
@@ -1,12 +1,10 @@
-
-
 import pygame
 pygame.init()
 screen=pygame.display.set_mode((333,500))
-bg=pygame.image.load("./img/下载.jpg")
+bg=pygame.image.load("img/下载.jpg")
 screen.blit(bg,(0,0))
 # pygame.display.update()
-cxk=pygame.image.load("./img/cxk.jpg")
+cxk=pygame.image.load("img/cxk.jpg")
 screen.blit(cxk,(150,460))
 pygame.display.update()
 cxk_rect=pygame.Rect(150,460,20,31)
@@ -14,6 +12,9 @@ cxk_rect=pygame.Rect(150,460,20,31)
 clock=pygame.time.Clock()
 while True:
     cxk_rect.y-=1
+    for i in pygame.event.get():
+        if i.type==pygame.QUIT:
+            exit()
     clock.tick(60)
     screen.blit(bg,(0,0))
     screen.blit(cxk,cxk_rect)
